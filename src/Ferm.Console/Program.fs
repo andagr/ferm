aa34dcc4d897ddcb9ea9b9e00900d87d7ba05f23
Andreas Ågren <bandagr@gmail.com>
2019-02-02 22:49:14 +0100

    Run arbitrary shell command

--- src/Ferm.Console/Program.fs
+++ src/Ferm.Console/Program.fs
@@ -1,8 +1,31 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿open System
+open System.Diagnostics
 open System
+
+let run command =
+  let timer = Stopwatch.StartNew()
+  let procStartInfo = 
+    ProcessStartInfo(
+      RedirectStandardOutput = true,
+      RedirectStandardError = true,
+      UseShellExecute = false,
+      CreateNoWindow = true,
+      FileName = "cmd.exe",
+      Arguments = sprintf "/C %s" command
+    )
+  use proc = new Process(StartInfo = procStartInfo)
+  proc.OutputDataReceived |> Event.add (fun data -> printfn "%s" (data.Data))
+  proc.ErrorDataReceived |> Event.add (fun data -> printfn "%s" (data.Data))
+  proc.Start() |> ignore
+  proc.BeginOutputReadLine()
+  proc.BeginErrorReadLine()
+  proc.WaitForExit()
+  timer.Stop()
+  printfn "%A" timer.Elapsed
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+  Console.Write("> ")
+  Console.ReadLine()
+  |> run
+  0 // return an integer exit code
